f9b67cef83c44031bc9d817ad9404df07302438b
Asmit Bhantana <bhantanasmit@gmail.com>
2017-05-13 12:16:58 +0545

    style article listing page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
-	validates :title, presence: true,length: {minimum:10, maximum:100}
-	validates :description, presence: true ,length: {minimum:10, maximum: 300}
+	validates :title, presence: true,length: {minimum:10, maximum:200}
+	validates :description, presence: true ,length: {minimum:10, maximum: 30000}
 end 
