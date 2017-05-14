5878024a4e26a0cc78c924b3b746b2950996fd23
Asmit Bhantana <bhantanasmit@gmail.com>
2017-05-14 14:29:17 +0545

    added user validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
-	validates :title, presence: true,length: {minimum:10, maximum:200}
-	validates :description, presence: true ,length: {minimum:10, maximum: 30000}
+	validates :title, presence: true,uniqueness: {case_sensitive: false},length: {minimum:10, maximum:200}
+	validates :description, presence: true ,uniqueness: {case_sensitive: false},length: {minimum:10, maximum: 30000}
 end 
