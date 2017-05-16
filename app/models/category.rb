9029043827ba4744accf99ee32f35f0b4c58aea2
Asmit Bhantana <bhantanasmit@gmail.com>
2017-05-16 18:43:59 +0545

    Added the category in navbar and then fixed some errors on test

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ActiveRecord::Base
  # validates :name, presence :true, length: {minimum:3, maximum:25}
  # validates_uniqueness_of :name
-  validates :name, presence: true, length: { minimum: 3, maximum: 25 }
+ 
+  validates :name, presence: true, uniqueness: {case_sensitive: false}, length: { minimum: 3, maximum: 25 }
   validates_uniqueness_of :name
 end
