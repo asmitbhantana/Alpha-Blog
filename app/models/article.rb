de20a2157d5b34d49c4331ce4b54d5fb9c0a85aa
Asmit Bhantana <bhantanasmit@gmail.com>
2017-05-16 19:03:00 +0545

    added some controllers

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ActiveRecord::Base
 	belongs_to :user
+	has_many  :article_categories
+	has_many  :categories, through: :article_categories
 	validates :title, presence: true,uniqueness: {case_sensitive: false},length: {minimum:10, maximum:200}
 	validates :description, presence: true ,uniqueness: {case_sensitive: false},length: {minimum:10, maximum: 30000}
 	validates :user_id, presence: true 
