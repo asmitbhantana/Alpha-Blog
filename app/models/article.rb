05c6f78322da7aa0009cc82bcef5b341cb0bc26e
Asmit Bhantana <bhantanasmit@gmail.com>
2017-05-20 17:38:21 +0545

    Added image in listing page and fixed align

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ActiveRecord::Base
 	belongs_to :user
 	has_many  :article_categories
 	has_many  :categories, through: :article_categories
-	has_attached_file :image, styles: { large: "600x600", medium: "300x300", thumb: "150x150"}
+	has_attached_file :image, styles: { large: "600x600", medium: "300x300", thumb: "150x150" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 	validates :title, presence: true,uniqueness: {case_sensitive: false},length: {minimum:10, maximum:200}
 	validates :description, presence: true ,uniqueness: {case_sensitive: false},length: {minimum:50, maximum:30000}
